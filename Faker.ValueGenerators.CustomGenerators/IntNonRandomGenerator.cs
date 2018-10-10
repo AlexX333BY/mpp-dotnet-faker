d64c74394b69eee39509b89bda360f0a6db3b084
Aliaksei <alexx333by@gmail.com>
2018-10-11 00:27:35 +0300

    Custom int generator implemented

---
+++ Faker.ValueGenerators.CustomGenerators/IntNonRandomGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Faker.ValueGenerators.BaseTypesGenerators;
+
+namespace Faker.ValueGenerators.CustomGenerators
+{
+    public class IntNonRandomGenerator : IBaseTypeGenerator
+    {
+        public Type GeneratedType
+        { get; set; }
+
+        public object Generate()
+        {
+            return 42;
+        }
+    }
+}
