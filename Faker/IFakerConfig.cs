c8887ae577af53117eea6c3d7160a17f367b7590
Aliaksei <alexx333by@gmail.com>
2018-10-10 13:13:14 +0300

    Faker config interface implemented

---
+++ Faker/IFakerConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+using Faker.ValueGenerators.BaseTypesGenerators;
+
+namespace Faker
+{
+    public interface IFakerConfig
+    {
+        void Add<T, U, W>(Expression<Func<T, U>> expression)
+            where W : IBaseTypeGenerator;
+    }
+}
