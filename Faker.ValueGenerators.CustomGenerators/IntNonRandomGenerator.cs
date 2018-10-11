4eea39b556d3cb1117eee4aea9235cc1a5c9216f
Alexey <alexx333by@gmail.com>
2018-10-11 09:55:44 +0300

    Added and implemented interface for non-random generators & reworked int non-random generator

--- Faker.ValueGenerators.CustomGenerators/IntNonRandomGenerator.cs
+++ Faker.ValueGenerators.CustomGenerators/IntNonRandomGenerator.cs
@@ -3,19 +3,27 @@ using Faker.ValueGenerators.BaseTypesGenerators;
 
 namespace Faker.ValueGenerators.CustomGenerators
 {
-    public class IntNonRandomGenerator : IBaseTypeGenerator
+    public class IntNonRandomGenerator : IBaseTypeGenerator, INonRandomGenerator
     {
         public Type GeneratedType
         { get; protected set; }
 
+        public int GeneratedValue
+        { get; protected set; }
+
         public object Generate()
         {
-            return 42;
+            return GeneratedValue;
         }
 
         public IntNonRandomGenerator()
+            : this(42)
+        { }
+
+        public IntNonRandomGenerator(int generatedValue)
         {
             GeneratedType = typeof(int);
+            GeneratedValue = generatedValue;
         }
     }
 }
