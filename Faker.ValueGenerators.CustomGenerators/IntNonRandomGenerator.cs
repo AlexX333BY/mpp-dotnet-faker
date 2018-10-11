05d078e6f6337030c8feeab551ac065310f9cdae
Alexey <alexx333by@gmail.com>
2018-10-11 09:05:57 +0300

    NonRandomIntGenerator improved

--- Faker.ValueGenerators.CustomGenerators/IntNonRandomGenerator.cs
+++ Faker.ValueGenerators.CustomGenerators/IntNonRandomGenerator.cs
@@ -6,11 +6,16 @@ namespace Faker.ValueGenerators.CustomGenerators
     public class IntNonRandomGenerator : IBaseTypeGenerator
     {
         public Type GeneratedType
-        { get; set; }
+        { get; protected set; }
 
         public object Generate()
         {
             return 42;
+        }
+
+        public IntNonRandomGenerator()
+        {
+            GeneratedType = typeof(int);
         }
     }
 }
