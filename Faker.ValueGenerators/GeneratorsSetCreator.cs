59fefbf052e992fc8a70530ac1ece573faf2d247
Alexey <alexx333by@gmail.com>
2018-10-12 19:37:14 +0300

    [Fix after review] Refactored generating sets of generators

--- Faker.ValueGenerators/GeneratorsSetCreator.cs
+++ Faker.ValueGenerators/GeneratorsSetCreator.cs
@@ -8,37 +8,28 @@ namespace Faker.ValueGenerators
 {
     public static class GeneratorsSetCreator
     {
+        private static void AddBaseGeneratorToDictionary(IBaseTypeGenerator generator, Dictionary<Type, IBaseTypeGenerator> dictionary)
+        {
+            dictionary.Add(generator.GeneratedType, generator);
+        }
+
         public static Dictionary<Type, IBaseTypeGenerator> CreateBaseTypesGeneratorsDictionary()
         {
             var dictionary = new Dictionary<Type, IBaseTypeGenerator>();
-            IBaseTypeGenerator generator;
 
-            generator = new BoolValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new ByteValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new DateTimeValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new DecimalValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new DoubleValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new FloatValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new IntValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new LongValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new SByteValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new ShortValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new UIntValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new ULongValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
-            generator = new UShortValueGenerator();
-            dictionary.Add(generator.GeneratedType, generator);
+            AddBaseGeneratorToDictionary(new BoolValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new ByteValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new DateTimeValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new DecimalValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new DoubleValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new FloatValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new IntValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new LongValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new SByteValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new ShortValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new UIntValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new ULongValueGenerator(), dictionary);
+            AddBaseGeneratorToDictionary(new UShortValueGenerator(), dictionary);
 
             return dictionary;
         }
