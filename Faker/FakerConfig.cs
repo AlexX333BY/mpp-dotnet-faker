b08cc6b276f32a3062d190d52542b5865cad67ea
Alexey <alexx333by@gmail.com>
2018-10-12 15:46:50 +0300

    [FakerConfig] Added checking generator generating value type

--- Faker/FakerConfig.cs
+++ Faker/FakerConfig.cs
@@ -20,14 +20,16 @@ namespace Faker
             where TGenerator : IBaseTypeGenerator, new()
         {
             Expression expressionBody = expression.Body;
-            if (expressionBody.NodeType == ExpressionType.MemberAccess)
-            {
-                generators.Add((PropertyInfo)((MemberExpression)expressionBody).Member, (IBaseTypeGenerator)Activator.CreateInstance(typeof(TGenerator)));
-            }
-            else
+            if (expressionBody.NodeType != ExpressionType.MemberAccess)
             {
                 throw new ArgumentException("Illegal expression");
             }
+            IBaseTypeGenerator generator = (IBaseTypeGenerator)Activator.CreateInstance(typeof(TGenerator));
+            if (!generator.GeneratedType.Equals(typeof(TPropertyType)))
+            {
+                throw new ArgumentException("Illegal generator");
+            }
+            generators.Add((PropertyInfo)((MemberExpression)expressionBody).Member, generator);
         }
 
         public FakerConfig()
